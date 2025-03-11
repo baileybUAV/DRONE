28d64a96348745e785acbc65e06e1881e0749ef5
rhaven222 <reesehaven@gmail.com>
2025-03-11 17:19:24 -0400

    new ip

--- communication/sockettest.py
+++ communication/sockettest.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
-client.connect(('172.28.11.51', 9999))
+client.connect(('10.42.0.1', 9999))
 
 client.send('Hello from Drone/UAV'.encode())
 print(client.recv(1024).decode())
