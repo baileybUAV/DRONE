e867a7ac7989b0088befe6b1e26e0491b0dfae8a
rhaven222 <reesehaven@gmail.com>
2025-03-11 17:33:58 -0400

    update

--- communication/sockettest.py
+++ communication/sockettest.py
@@ -2,9 +2,11 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.connect(('10.42.0.1', 9999))
-
+print("Sending Message")
 client.sendto('Hello from Drone/UAV'.encode(), ('10.42.0.22', 9999))
-
+print("Message Sent")
 data, addr = client.recvfrom(1024)
 
 print(data.decode())
+
+exit()
