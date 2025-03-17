9a46d4be1a5b7df88aa3371621c0be583c83c701
rhaven222 <reesehaven@gmail.com>
2025-03-17 16:04:18 -0400

    cv capture

--- OpenCV/image_capture.py
+++ OpenCV/image_capture.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     config = picam2.create_preview_configuration(main={"size": (1280, 720), "format": "RGB888"})
     picam2.configure(config)
     picam2.start()
-    source = picam2.capture_array()
-    #source = cv2.VideoCapture(0)
+    source1 = picam2.capture_array()
+    source = cv2.VideoCapture(source1)
     # square_size = float(args.get('--square_size', 1.0))
     
     pattern_size = (9, 6)
