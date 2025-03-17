26d7b7eab5ec452fc9a5e7b00a8d3c19ae2c0ddb
rhaven222 <reesehaven@gmail.com>
2025-03-17 16:44:40 -0400

    1

--- OpenCV/Aruco_Detection.py
+++ OpenCV/Aruco_Detection.py
@@ -44,7 +44,7 @@ marker_corners_3d = np.array([
 detector = HomogeneousBgDetector(aruco_dict, parameters)
 
 # Open webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1, cv2.CAP_V4L2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
