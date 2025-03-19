b3ab71169333f8ae956583f55f8f633f3a1ac162
rhaven222 <reesehaven@gmail.com>
2025-03-19 13:30:00 -0400

    telem

--- communication/telem_test.py
+++ communication/telem_test.py
@@ -66,7 +66,11 @@ while True:
         int(lat * 1e7),  # Latitude in 1E7 degrees
         int(lon * 1e7),  # Longitude in 1E7 degrees
         int(alt * 1000),  # Altitude in mm
-        0, 0, 0, 0, 0  # Dummy values for accuracy and velocity
+        int(vehicle.gps_0.eph),  # HDOP (horizontal accuracy in cm)
+        int(vehicle.gps_0.epv),  # VDOP (vertical accuracy in cm)
+        int(vehicle.groundspeed * 100),  # Velocity in cm/s
+        int(vehicle.heading * 100),  # Course over ground (degrees * 100)
+        vehicle.gps_0.satellites_visible  # Number of satellites visible
     )
     
     # Send the message to the other Pi
