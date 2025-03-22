741c1457db57107b8f886dd0dbaf4ac4d8ef3d22
rhaven222 <reesehaven@gmail.com>
2025-03-22 17:30:00 -0400

    Test Flights/percision_land.py

--- Test Flights/percision_land.py
+++ Test Flights/percision_land.py
@@ -97,24 +97,24 @@ def send_land_message(x, y):
     msg = vehicle.message_factory.landing_target_encode(
         0,
         0,
-        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+        mavutil.mavlink.MAV_FRAME_BODY_FRD,
         x, y,
         0, 0, 0)
     vehicle.send_mavlink(msg)
     vehicle.flush()
 
-def send_local_ned_velocity(vx, vy, vz):
-	msg = vehicle.message_factory.set_position_target_local_ned_encode(
-		0,
-		0, 0,
-		mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
-		0b0000111111000111,
-		0, 0, 0,
-		vx, vy, vz,
-		0, 0, 0,
-		0, 0)
-	vehicle.send_mavlink(msg)
-	vehicle.flush()
+def send_body_ned_velocity(vx, vy, vz):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0, 0,
+        mavutil.mavlink.MAV_FRAME_BODY_FRD,
+        0b0000111111000111,
+        0, 0, 0,
+        vx, vy, vz,
+        0, 0, 0,
+        0, 0)
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
 
 # ------------------- PRECISION LANDING -------------------
 def precision_land():
