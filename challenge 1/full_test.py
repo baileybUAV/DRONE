f4f6c838c41c02f7d18422d8cb812388c9c70715
rhaven222 <reesehaven@gmail.com>
2025-04-06 17:46:51 -0400

    test

--- challenge 1/full_test.py
+++ challenge 1/full_test.py
@@ -17,7 +17,7 @@ import argparse
 # ------------------- CONFIG -------------------
 takeoff_altitude = 3  # meters
 camera_resolution = (1600, 1080)
-marker_id = 0
+marker_id = 4
 marker_size = 0.253  # meters
 descent_speed = 0.2
 final_land_height = 1.0  # meters
