cd0f9e8a0c328712b11bfa825e878ca0a30eb6cd
rhaven222 <reesehaven@gmail.com>
2025-04-12 16:36:04 -0400

    test

--- challenge 2/test1.py
+++ challenge 2/test1.py
@@ -215,42 +215,41 @@ def precision_land_pixel_offset():
                 vehicle.mode = VehicleMode("LAND")
                 time.sleep(3)
                 print("Starting data transmission...")
-                start_time = time.time()
-                while time.time() - start_time < 30:
-                    if vehicle.gps_0.fix_type != 6:
-                         print("Error: GPS does not have RTK Fixed")
-                         print("GPS STATUS: %s" % vehicle.gps_0.fix_type)
-                    else:
-                        #LOG LOCATION
-                        lat = vehicle.location.global_frame.lat
-                        lon = vehicle.location.global_frame.lon
-                        alt = vehicle.location.global_frame.alt
-                        rel_alt = vehicle.location.global_relative_frame.alt
-                        velocity_north = vehicle.velocity[0]
-                        velocity_east = vehicle.velocity[1]
-                        velocity_down = vehicle.velocity[2]
-                        covariance_matrix = np.full((36,), float('nan'), dtype=np.float32)
-                       
-                        msg = telem_link.mav.global_position_int_cov_encode(
-                            int(time.time() * 1e6),
-                            mavutil.mavlink.MAV_ESTIMATOR_TYPE_GPS,
-                                int(lat * 1e7),
-                                int(lon * 1e7),
-                                int(alt * 1000),
-                                int(rel_alt * 1000),
-                                float(velocity_north),
-                                float(velocity_east),
-                                float(velocity_down),
-                                covariance_matrix)
 
-                        telem_link.mav.send(msg)
-                        print(f"Sent Aruco Location Data: Lat {lat}, Lon {lon}, Alt {alt}, VelN {velocity_north}, VelE {velocity_east}, VelD {velocity_down}")
-                        logger.info(f"Transmitting DropZone Aruco Location Data TO UGV: Lat {lat}, Lon {lon}, Alt {alt}")
-                        time.sleep(1)  # Send every 1 seconds
-                        print("DropZone Location Sent!")
+                if vehicle.gps_0.fix_type != 6:
+                    print("Error: GPS does not have RTK Fixed")
+                    print("GPS STATUS: %s" % vehicle.gps_0.fix_type)
+                else:
+                    #LOG LOCATION
+                    lat = vehicle.location.global_frame.lat
+                    lon = vehicle.location.global_frame.lon
+                    alt = vehicle.location.global_frame.alt
+                    rel_alt = vehicle.location.global_relative_frame.alt
+                    velocity_north = vehicle.velocity[0]
+                    velocity_east = vehicle.velocity[1]
+                    velocity_down = vehicle.velocity[2]
+                    covariance_matrix = np.full((36,), float('nan'), dtype=np.float32)
+                       
+                    msg = telem_link.mav.global_position_int_cov_encode(
+                        int(time.time() * 1e6),
+                        mavutil.mavlink.MAV_ESTIMATOR_TYPE_GPS,
+                            int(lat * 1e7),
+                            int(lon * 1e7),
+                            int(alt * 1000),
+                            int(rel_alt * 1000),
+                            float(velocity_north),
+                            float(velocity_east),
+                            float(velocity_down),
+                            covariance_matrix)
+
+                    telem_link.mav.send(msg)
+                    print(f"Sent Aruco Location Data: Lat {lat}, Lon {lon}, Alt {alt}, VelN {velocity_north}, VelE {velocity_east}, VelD {velocity_down}")
+                    logger.info(f"Transmitting DropZone Aruco Location Data TO UGV: Lat {lat}, Lon {lon}, Alt {alt}")
+                    time.sleep(1)  # Send every 1 seconds
+                    print("DropZone Location Sent!")
                         
                 logger.info("DropZone Location Has been Transmitted to UGV")
-                print("Transmission Time has Elapsed")
+                print("Transmission has Elapsed")
                 print("Returning to Launch Point...")
                 vehicle.mode = VehicleMode("GUIDED")
                 vehicle.armed = True
