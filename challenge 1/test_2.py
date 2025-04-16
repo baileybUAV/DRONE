e590a44b3a433b09939fdb811cc59da3f72f8213
rhaven222 <reesehaven@gmail.com>
2025-04-16 17:47:25 -0400

    test

--- challenge 1/test_2.py
+++ challenge 1/test_2.py
@@ -24,7 +24,7 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 # ------------------- CONFIG -------------------
-takeoff_altitude = 6  # meters
+takeoff_altitude = 7  # meters
 camera_resolution = (1600, 1080)
 marker_id = 3
 marker_size = 0.253  # meters
