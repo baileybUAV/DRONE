c4a61d07098e850aed0014cf5044ef6ba36afcf8
rhaven222 <reesehaven@gmail.com>
2025-04-17 15:29:02 -0400

    test

--- challenge 1/test_2.py
+++ challenge 1/test_2.py
@@ -140,12 +140,12 @@ def marker_watcher():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
         if ids is not None and marker_id in ids:
+            marker_found_flag.set()
             capture_photo(0)
             print("DropZone FOUND! Triggering landing...")
             logger.info("DropZone Found")
             send_ned_velocity(-2.5,0,1.5)
             time.sleep(2)
-            marker_found_flag.set()
             aruco_lat = vehicle.location.global_frame.lat
             aruco_lon = vehicle.location.global_frame.lon
             print(f"DropZone Location: Lat {aruco_lat}, Lon {aruco_lon} assigned to {marker_id}")
