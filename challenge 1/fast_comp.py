168ac784f3b96475e62c601debdfedb280ceb9ac
rhaven222 <reesehaven@gmail.com>
2025-04-19 16:57:35 -0400

    test

--- challenge 1/fast_comp.py
+++ challenge 1/fast_comp.py
@@ -26,7 +26,7 @@ logger = logging.getLogger()
 # ------------------- CONFIG -------------------
 takeoff_altitude = 4  # meters
 camera_resolution = (1600, 1080)
-marker_id = 0
+marker_id = 3
 marker_size = 0.253  # meters
 descent_speed = 0.2
 final_land_height = 1.5  # meters
