012dcf61a581e1492eca23a42651d51c92383faa
rhaven222 <reesehaven@gmail.com>
2025-04-21 17:29:35 -0400

    test

--- challenge 1/test_5.py
+++ challenge 1/test_5.py
@@ -246,7 +246,7 @@ def precision_land_pixel_offset():
                 break
         else:
             print("Marker Lost. Returning to last known location")
-            vehicle.simple_goto(LocationGlobalRelative(aruco_lat, aruco_lon, takeoff_altitude + 2))
+            vehicle.simple_goto(LocationGlobalRelative(aruco_lat, aruco_lon, takeoff_altitude + 1.5))
         time.sleep(0.1)
 
 # ------------------- MAIN MISSION -------------------
